f0ded1317d6c792c1d57db0aa499c32b083e60b7
Jacky Boen <aqiank@gmail.com>
2015-04-21 11:38:50 +0800

    Added basic encryption test
    Signed-off-by: Jacky Boen <aqiank@gmail.com>

---
+++ enigma_test.go
@@ -0,0 +1,23 @@
+package enigma
+
+import (
+	"testing"
+)
+
+const (
+	Input = "Hello World."
+	Output = "QRIGBRXSWC"
+)
+
+func TestEncrypt(t *testing.T) {
+	pb := NewComponent(Plugboard)
+	r1 := NewComponent(Rotor)
+	r2 := NewComponent(Rotor)
+	r3 := NewComponent(Rotor)
+	rf := NewComponent(Reflector)
+	pb.Connect(r1).Connect(r2).Connect(r3).Connect(rf)
+	output := pb.Encrypt([]byte(Input))
+	if string(output) != Output {
+		t.Fatal("Encrypted text doesn't matched intended output")
+	}
+}
